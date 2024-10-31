4c40e3cd84ccdf05faee001f47311f89c57a0aec
yvan-nguekam <nguekamyvan4@gmail.com>
2024-10-31 12:35:05 +0100

    using selctor strategy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "media", // add this line
+  darkMode: "selector", // add this line
   theme: {
     extend: {},
   },
