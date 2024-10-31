1d69a0c9b0dcc2c73dd98f065cd48e90a6ff10c2
yvan-nguekam <nguekamyvan4@gmail.com>
2024-10-31 12:38:44 +0100

    supporting system preference and manual selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,60 @@
 "use client";
-import { useEffect } from "react";
+
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [activeTheme, setActiveTheme] = useState<string>("system");
+
   useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    if (theme === "dark") {
-      document.documentElement.classList.add("dark");
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "system" || !savedTheme) {
+      applySystemTheme();
+      setActiveTheme("system");
+    } else {
+      applyTheme(savedTheme);
+      setActiveTheme(savedTheme);
     }
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleSystemThemeChange = () => {
+      if (!savedTheme || savedTheme === "system") {
+        applySystemTheme();
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleSystemThemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleSystemThemeChange);
+    };
   }, []);
 
-  const toggleTheme = () => {
-    if (document.documentElement.classList.contains("dark")) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    } else {
+  const applyTheme = (theme: string) => {
+    if (theme === "dark") {
       document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
+    } else if (theme === "light") {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  const handleThemeChange = (newTheme: string) => {
+    setActiveTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    if (newTheme === "system") {
+      applySystemTheme();
+    } else {
+      applyTheme(newTheme);
+    }
+  };
+
+  const applySystemTheme = () => {
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (systemPrefersDark) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
     }
   };
 
@@ -31,12 +70,39 @@ export default function Home() {
           exercitationem quisquam illo omnis? Fuga, voluptates? Eum dolor ipsam
           expedita perspiciatis doloremque, ad illo!
         </p>
-        <button
-          onClick={toggleTheme}
-          className="flex justify-center items-center m-auto text-lg w-fit dark:bg-sky-500/50 bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-10 rounded-lg text-gray-50 font-semibold py-[10px] px-4"
-        >
-          Toggle Theme
-        </button>
+
+        <div className="flex items-center">
+          <button
+            onClick={() => handleThemeChange("light")}
+            className={`flex justify-center items-center m-auto text-lg w-fit transition-color duration-200 ease-in-out py-3 px-10 rounded-lg font-semibold py-[10px] px-4 ${
+              activeTheme === "light"
+                ? "bg-cyan-800 text-gray-50 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300 hover:bg-slate-600 text-gray-50"
+                : "bg-slate-500 dark:bg-slate-600 hover:bg-cyan-800 text-gray-100 dark:text-gray-400"
+            }`}
+          >
+            Light Theme
+          </button>
+          <button
+            onClick={() => handleThemeChange("dark")}
+            className={`flex justify-center items-center m-auto text-lg w-fit transition-color duration-200 ease-in-out py-3 px-10 rounded-lg font-semibold py-[10px] px-4 ${
+              activeTheme === "dark"
+                ? "bg-cyan-800 text-gray-50 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300 hover:bg-slate-600 text-gray-50"
+                : "bg-slate-500 dark:bg-slate-600 hover:bg-cyan-800 text-gray-50 dark:text-gray-400"
+            }`}
+          >
+            Dark Theme
+          </button>
+          <button
+            onClick={() => handleThemeChange("system")}
+            className={`flex justify-center items-center m-auto text-lg w-fit transition-color duration-200 ease-in-out py-3 px-10 rounded-lg font-semibold py-[10px] px-4 ${
+              activeTheme === "system"
+                ? "bg-cyan-800 text-gray-50 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300 hover:bg-slate-600 text-gray-50"
+                : "bg-slate-500 dark:bg-slate-600 hover:bg-cyan-800 text-gray-50 dark:text-gray-400"
+            }`}
+          >
+            Use System Theme
+          </button>
+        </div>
       </div>
     </main>
   );
